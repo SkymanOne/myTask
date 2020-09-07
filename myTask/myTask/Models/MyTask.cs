9fc451570af9c78ad289c13f88c55222a5a07ae7
German <german.nikolishin@gmail.com>
2020-09-07 15:32:09 +0100

    myTask model update and manager finalisation

--- myTask/myTask/Models/MyTask.cs
+++ myTask/myTask/Models/MyTask.cs
@@ -16,12 +16,21 @@ namespace myTask.Models
         [ManyToMany(typeof(MyTaskTag))]
         public virtual ICollection<Tag> Tags { get; set; }
         public List<string> SubTasks { get; set; }
-        public double PriorityLevel { get; set; }
-        
-        //Calculated manually
+        //set by user
+        public double Importance { get; set; }
+        //Calculated automatically
         public double PriorityCoefficient { get; set; }
+        //set automatically as a hint for the user
+        public PriorityLevel PriorityLevel { get; set; }
         public int Kinbens { get; set; }
         public DateTime Deadline { get; set; }
         public int DurationMinutes { get; set; }
+    }
+
+    public enum PriorityLevel
+    {
+        Low,
+        Medium,
+        High
     }
 }
--- myTask/myTask/Services/MyTasksManager/IMyTaskManager.cs
+++ myTask/myTask/Services/MyTasksManager/IMyTaskManager.cs
@@ -4,15 +4,21 @@ using myTask.Models;
 
 namespace myTask.Services.MyTasksManager
 {
+    /// <summary>
+    /// Interface describing the basic functionality of the core
+    /// Contains algorithms and equations for task management
+    /// </summary>
     public interface IMyTaskManager
     {
         Task<ICollection<MyTask>> LoadAllTasksAsync();
-        Task<ICollection<MyTask>> LoadWeeklyTimetable(int weekOfTheYear);
-        Task<ICollection<MyTask>> LoadTasksAsync(Weekday day, int weekOfTheYear);
-        Task<ICollection<MyTask>> LoadTasksAsync(Weekday day);
+        Task<ICollection<MyTask>> LoadWeeklyTimetableAsync(int weekOfTheYear);
+        Task<ICollection<MyTask>> LoadTasksAsync(Weekday day, int weekOfTheYear = -1);
         Task<ICollection<MyTask>> LoadTasksAsync();
-
         Task<bool> CreateTaskAsync(MyTask myTask);
         Task<bool> UpdateTaskAsync(MyTask myTask);
+        Task<List<MyTask>> SplitTaskAsync(MyTask myTask);
+        Task<bool> MoveTaskForwardAsync(MyTask myTask);
+        Task<bool> MoveTaskBackwardsAsync(MyTask myTask);
+        Task UpdateTimetableAsync();
     }
 }
